76deef426d617da741b19ad1e2f2da7ff272d937
Siddhi-D-Nagarkar <siddhinagarkar1@gmail.com>
2024-12-14 11:58:48 +0530

    Initial commit

---
+++ src/test/java/com/example/UserCreation/UserCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserCreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
